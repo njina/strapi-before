82459492736be0fa720114c3f1d12fc8b67e6507
njina <najina@gmail.com>
2022-04-06 00:08:35 +0900

    create new content-type-builder Gnb

---
+++ src/api/gnb/routes/gnb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gnb router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::gnb.gnb');
---
+++ src/api/gnb/services/gnb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gnb service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::gnb.gnb');
